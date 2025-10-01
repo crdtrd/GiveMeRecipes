ff6d87aaa16e0e72acd5e588806585b72adf3f67
crdtrd <crdtrd@gmail.com>
2025-09-30 22:28:51 -0700

    start

---
+++ src/main/java/com/drtdrc/givemerecipes/GiveMeRecipes.java
@@ -0,0 +1,10 @@
+package com.drtdrc.givemerecipes;
+
+import net.fabricmc.api.ModInitializer;
+
+public class GiveMeRecipes implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
